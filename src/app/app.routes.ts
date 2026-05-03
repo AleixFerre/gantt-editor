2367e04f6c1828ceb43436b3b3d256c0b4cfc54c
AleixFerreCP <Aleix.ferre@clearpeaks.com>
2026-05-03 20:23:55 +0200

    login first

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,15 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    loadComponent: () => import('./login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'app',
     pathMatch: 'full',
     loadComponent: () => import('./gantt/gantt.component').then((m) => m.GanttComponent),
   },
   {
     path: '**',
-    loadComponent: () =>
-      import('./not-found/not-found.component').then((m) => m.NotFoundComponent),
+    loadComponent: () => import('./not-found/not-found.component').then((m) => m.NotFoundComponent),
   },
 ];
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,24 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule, RouterLink],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  private readonly fb = inject(FormBuilder);
+
+  protected readonly form = this.fb.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  protected submit(): void {
+    if (this.form.invalid) return;
+    // TODO: wire up authentication
+  }
+}
