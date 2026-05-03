3b7b66086578dc51c580b862bc4ab4f63cbcdb73
AleixFerreCP <Aleix.ferre@clearpeaks.com>
2026-05-03 13:26:05 +0200

    404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,12 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full',
     loadComponent: () => import('./gantt/gantt').then((m) => m.GanttComponent),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./not-found/not-found').then((m) => m.NotFoundComponent),
   },
 ];
